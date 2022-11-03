3e6a1fe1dac5a02bae0086204c1ac60e5ddfd6df
Ikemefuna Nwodo <Ikemefuna.nwodo@globalaccelerex.com>
2022-11-03 16:48:52 +0100

    Script for csv and json

---
+++ src/main/java/com/ikem/hng_backend_bidding_task/HngBackendBiddingTaskApplication.java
@@ -0,0 +1,91 @@
+package com.ikem.hng_backend_bidding_task;
+
+
+import com.google.common.hash.Hashing;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.json.CDL;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class HngBackendBiddingTaskApplication {
+
+    public static void main(String[] args) {
+        //csvToJson();
+        readCSV();
+    }
+
+    static void csvToJson() {
+        // creates an object of Scanner
+        Scanner input = new Scanner(System.in);
+        //InputStream inputStream = HngBackendBiddingTaskApplication.class.getClassLoader().getResourceAsStream(input.nextLine());
+        System.out.print("Enter file path:");
+        String filePath = input.nextLine();
+
+        try {
+            FileInputStream fstream = new FileInputStream(filePath);
+            DataInputStream in = new DataInputStream(fstream);
+            String csvAsString = new BufferedReader(new InputStreamReader(in)).lines().collect(Collectors.joining("\n"));
+            System.out.println(csvAsString);
+            String json = CDL.toJSONArray(csvAsString).toString();
+            Files.write(Path.of("output.json"), json.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void readCSV() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter file path:");
+        String filePath = input.nextLine();
+
+        new File("/json").mkdirs();
+
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] lineInArray;
+            String columns = String.join(",", reader.peek());
+            System.out.println(columns);
+            int counter = 0;
+            while ((lineInArray = reader.readNext()) != null) {
+
+
+                if (counter <= 0) {
+                    counter++;
+                    continue;
+                }
+
+                String filename = "/json/" + lineInArray[1].trim() + ".json";
+
+
+
+                System.out.println(filename);
+                String content = String.join(",", lineInArray);
+
+                String csvString = "format,"+ columns + "\n" + "CHIP-0007," + content;
+                System.out.println(csvString);
+                String json = CDL.toJSONArray(csvString).toString().
+                        replace("[", "")
+                        .replace("]", "");
+                System.out.println(json);
+                Files.write(Path.of(filename), json.getBytes(StandardCharsets.UTF_8));
+
+            }
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static String jsonFileHash(String jsonFilename) {
+        return Hashing.sha256()
+                .hashString(jsonFilename, StandardCharsets.UTF_8)
+                .toString();
+    }
+}
---
+++ src/test/java/com/ikem/hng_backend_bidding_task/HngBackendBiddingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikem.hng_backend_bidding_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngBackendBiddingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
